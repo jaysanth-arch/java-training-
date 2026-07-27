4b23f4fd9c389e3db627ae88c594c676c1814754
jaysanth-arch <jaysanth2605@gmail.com>
2026-07-27 16:45:42 +0530

    recursion and the day5 task

---
+++ sumviaRecursion.java
@@ -0,0 +1,11 @@
+public class sumviaRecursion {
+    public static double findsum (double[] arr, int index){
+        if(index==0) return arr[index];
+        return arr[index]+findsum (arr,index-1);
+    }
+    public static void main(String[] args) {
+        double[] onRoad ={7.45,13.5,4.5,21.6};
+        double sum = findsum (onRoad ,onRoad.length-1);
+        System.out.println(sum);
+    }
+}
