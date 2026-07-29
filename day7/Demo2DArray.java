e279f50f1364aabd13129b40f703d847604b2dbb
jaysanth-arch <jaysanth2605@gmail.com>
2026-07-29 20:38:23 +0530

    day7 session

---
+++ day7/Demo2DArray.java
@@ -0,0 +1,27 @@
+package day7;
+
+import java.util.Arrays;
+
+public class Demo2DArray {
+    public static void traverse(double[][] arr){
+        Arrays.stream(arr).forEach(
+            each->System.out.println(
+                Arrays.toString(each))
+        );
+    }
+    public static void main(String[] args) {
+        double[][] salary = {
+            {3.4,9.2,12.5},
+            {1.9,5.6,25.9,4.5},
+            {22.5,4.1},
+            {7.3,3.8,4.1,8.5,32.8,12.5,17.9,1.5}
+        };
+        traverse(salary);
+        System.out.println(salary[0][0]+" "+salary[1][0]+" "+salary[2][0]);
+        double[][] asset = new double[3][2];
+        asset[0] = new double[]{9.3,12.5};
+        asset[1] = new double[]{8.4,6.7};
+        asset[2] = new double[]{45.3,76.5};
+
+}
+    }
